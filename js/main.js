3e25ca567472e26607650d09a828955aca543211
leonelarian <duck.arin@yahoo.com>
2022-11-22 17:41:18 -0300

    commit 1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function sitionocturno() 
+    {
+         document.body.style.backgroundColor = "black";
+         document.getElementById("section").style.color = "whitesmoke";
+         document.getElementById("header").style.backgroundColor = "black";
+         document.getElementById("aside").style.backgroundColor = "black";
+         document.getElementById("main").style.backgroundColor = "whitesmoke";
+         document.getElementById("nav").style.backgroundColor = "black";
+         document.getElementById("footer").style.backgroundColor = "black";
+         document.getElementById("footer").style.color = "whitesmoke";
+         document.getElementById("header").style.color = "whitesmoke";
+         document.getElementById("section").style.color = "green";
+    }
+
+function copy() {
+     alert("Autor: Arián Leonel Duck. EMAIL: duck.arin@yahoo.com");
+     }
